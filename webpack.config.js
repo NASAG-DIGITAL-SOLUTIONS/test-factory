f624e7773bfbcf1de1d8fbf161d2661ee9834622
Cherif Bekhtari <mobekhtari@gmail.com>
2024-01-07 14:07:54 +0100

    refactor(core): adding existing conf. files and ci-cd workflows
    #1

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import path, { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default {
+    entry: ['./src/index.js'],
+    mode: 'production',
+    externals: 'window',
+    output: {
+        path: resolve(__dirname, 'umd'),
+        filename: 'core.min.js',
+        library: '@testfactory/core',
+        libraryTarget: 'umd'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, // instead of style-loader
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: 'core.styles.css' })]
+}
